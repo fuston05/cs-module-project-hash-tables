985f5f7a2942f23eaa24a72b781e7ea9dc53de5e
fuston05 <fuston05@yahoo.com>
2020-08-05 17:50:55 -0400

    passes word_count.py test

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,34 @@
 
 def word_count(s):
-    countedWords= {}
+    ignoredChars = [
+        "?", "!", "\"", ":", ";", ",", ".", "-", "+", "=", "/", "\\", "|", "[", "]", "{", "}", "(", ")", "*", "^", "&"
+    ]
 
+    countedWords = {}
+
+    s = s.lower()
+
+    # check for special chars and remove,
+    ignoredCount = 0
+    for i in s:
+        for c in ignoredChars:
+            if i == c:
+                ignoredCount += 1
+                s = s.replace(i, '')
+
+    # if no special chars return NONE
+    if ignoredCount == 0 and len(s) == 0:
+        return {}
+
+    # build look-up table
+    word_list = s.split()
+    for word in word_list:
+        if word not in countedWords:
+            countedWords[word] = 1
+        else:
+            countedWords[word] += 1
+
+    return countedWords
 
 
 if __name__ == "__main__":
@@ -9,7 +36,7 @@ if __name__ == "__main__":
     # print(word_count(""))
     print('')
     # print(word_count("Hello    hello"))
-    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+    # print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print('')
     # print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print('')
